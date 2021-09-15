85fa9b8645c5d00300a0d3b6ad298e36601aa0a2
廖政 <813144314@qq.com>
2021-09-15 10:03:29 +0800

    feat: 运用async setup函数解决服务端预获取数据的渲染问题

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -6,8 +6,15 @@
 import { defineComponent, ref } from "vue";
 
 export default defineComponent({
-  setup() {
+  async setup() {
     let list = ref();
+    const res = await new Promise((resolve) => {
+      //you can inject api request here
+      setTimeout(() => {
+        resolve(["11", "22", "33", "44", "55", "66"]);
+      }, 2000);
+    });
+    list.value = res;
     return {
       list,
     };
